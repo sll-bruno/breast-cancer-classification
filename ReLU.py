0769a3a1a16c5a5577829d32abae301eed9df8a0
Isabel Cristina <isabelmarras13@gmail.com>
2026-05-25 18:17:46 -0300

    cria classe pai de função de ativação

--- ReLU.py
+++ ReLU.py
@@ -1,33 +1,33 @@
-import numpy as np
+# import numpy as np
 
-class ReLU:
-    """
-    Implementação de Foward e Backward para função de ativação ReLU
-    """
-    def forward(self, inputs):
-        """
-        Aplicação da função de ativação sobre os valores de input
-        ReLU(x) = max(0, x)
-        """
-        self.inputs = inputs
-        self.output = np.maximum(0, inputs)
+# class ReLU:
+#     """
+#     Implementação de Foward e Backward para função de ativação ReLU
+#     """
+#     def forward(self, inputs):
+#         """
+#         Aplicação da função de ativação sobre os valores de input
+#         ReLU(x) = max(0, x)
+#         """
+#         self.inputs = inputs
+#         self.output = np.maximum(0, inputs)
 
-    def backwards(self, dvalues):
-        """
-        Passa 'para trás' o gradiente apenas onde os neuronios estavam ativos
-        Entrada:
-        - dvalues: Gradiente da camada seguinte
-        Saída:
-        - dinputs: Gradiente da camada atual que será passada para a anterior
+#     def backwards(self, dvalues):
+#         """
+#         Passa 'para trás' o gradiente apenas onde os neuronios estavam ativos
+#         Entrada:
+#         - dvalues: Gradiente da camada seguinte
+#         Saída:
+#         - dinputs: Gradiente da camada atual que será passada para a anterior
 
-        O cálculo do gradiente é realizado a partir da regra da cadeia aplicada sobre a ReLU.
-        """
+#         O cálculo do gradiente é realizado a partir da regra da cadeia aplicada sobre a ReLU.
+#         """
 
-        #Copia os valores dos gradientes da camada seguinte
-        self.dinputs = dvalues.copy()
+#         #Copia os valores dos gradientes da camada seguinte
+#         self.dinputs = dvalues.copy()
 
-        # Aplica a derivada da ReLU: se o valor de entrada for menor ou igual a 0,
-        # o gradiente é 0,caso contrário, é mantido o valor do gradiente da camada seguinte.
-        self.dinputs[self.inputs <= 0] = 0
+#         # Aplica a derivada da ReLU: se o valor de entrada for menor ou igual a 0,
+#         # o gradiente é 0,caso contrário, é mantido o valor do gradiente da camada seguinte.
+#         self.dinputs[self.inputs <= 0] = 0
 
-        return self.dinputs
+#         return self.dinputs
---
+++ activation_function.py
@@ -0,0 +1,79 @@
+import numpy as np
+
+class ActivationFunction():
+    def forward(self, Z):
+        raise NotImplementedError
+        
+    def backward(self, dA):
+        raise NotImplementedError   
+    
+class ReLU(ActivationFunction):
+    """
+    Implementação de Foward e Backward para função de ativação ReLU
+    """
+    def forward(self, inputs):
+        """
+        Aplicação da função de ativação sobre os valores de input
+        ReLU(x) = max(0, x)
+        """
+        self.inputs = inputs
+        self.output = np.maximum(0, inputs)
+
+    def backward(self, dvalues):
+        """
+        Passa 'para trás' o gradiente apenas onde os neuronios estavam ativos
+        Entrada:
+        - dvalues: Gradiente da camada seguinte
+        Saída:
+        - dinputs: Gradiente da camada atual que será passada para a anterior
+
+        O cálculo do gradiente é realizado a partir da regra da cadeia aplicada sobre a ReLU.
+        """
+
+        #Copia os valores dos gradientes da camada seguinte
+        self.dinputs = dvalues.copy()
+
+        # Aplica a derivada da ReLU: se o valor de entrada for menor ou igual a 0,
+        # o gradiente é 0,caso contrário, é mantido o valor do gradiente da camada seguinte.
+        self.dinputs[self.inputs <= 0] = 0
+
+        return self.dinputs
+    
+
+class SoftmaxCrossEntropy(ActivationFunction):
+    def __init__(self):
+        self.A = None 
+
+    def forward(self, Z, Y_true):
+        """
+        Z e Y_true shape: (num_classes, batch_size)
+        """
+        # tal do overflow
+        Z_shifted = Z - np.max(Z, axis=0, keepdims=True)
+        exp_Z = np.exp(Z_shifted)
+        
+        # aqui é para calcular as probabilidades
+        # Formula do AW
+        self.A = exp_Z / np.sum(exp_Z, axis=0, keepdims=True)
+        
+        # Calcula a Cross-Entropy Loss escalar
+        m = Y_true.shape[1]
+        log_probs = np.log(self.A + 1e-8)
+        loss = - (1 / m) * np.sum(Y_true * log_probs)
+        
+        return loss
+
+    # Observação no Z
+    def backward(self, Z, Y_true):
+        """
+        Retorna o gradiente dZ para a última camada Dense.
+        """
+        dZ = self.A - Y_true
+        return dZ
+    
+    def predict(self, Z):
+        Z_shifted = Z - np.max(Z, axis=0, keepdims=True)
+        exp_Z = np.exp(Z_shifted)
+        
+        probabilidades = exp_Z / np.sum(exp_Z, axis=0, keepdims=True)
+        return probabilidades
